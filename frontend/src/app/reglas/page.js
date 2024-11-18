16069105c8563143cc3372b44f2d4535dc81ce4f
FranRebe <164498259+FranRebe@users.noreply.github.com>
2024-11-18 15:53:35 -0300

    Fondo de pantalla en reglas
    Fondo de imagen en reglas

--- frontend/src/app/reglas/page.js
+++ frontend/src/app/reglas/page.js
@@ -1,19 +1,10 @@
 import Reglas from "../components/reglas";
 import BotonDeJuego from "../components/boton";
+import styles from './Reglitas.module.css';
 
 export default function Reglitas() {
-  // Estilos para el contenedor
-  const containerStyle = {
-    backgroundColor: "#007BFF", // Fondo azul
-    display: "flex",
-    justifyContent: "center", // Centrar horizontalmente
-    alignItems: "center", // Centrar verticalmente
-    minHeight: "100vh", // Asegura que ocupe toda la altura de la ventana
-    flexDirection: "column", // Para que el botón esté debajo de las reglas
-  };
-
   return (
-    <div style={containerStyle}>
+    <div className={styles.page}>
       <Reglas />
       <BotonDeJuego />
     </div>
